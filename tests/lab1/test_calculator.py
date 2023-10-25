728cd006a1716118cea31a5c24463663478f6cbb
teneramite <kiselsofia3@gmail.com>
2023-10-25 09:34:31 -0300

    Добавлено решеніе

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,35 @@
 import unittest
+import calculator_made_with_love 
 
-class CalculatorTestCase(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+    def test_addition(self):
+        self.assertEqual(calculator_made_with_love.addition(2, 2), 4)
+        self.assertEqual(calculator_made_with_love.addition(-2, 2), 0)
+        self.assertEqual(calculator_made_with_love.addition(-2, -2), -4)
+
+    def test_subtraction(self):
+        self.assertEqual(calculator_made_with_love.subtraction(2, 2), 0)
+        self.assertEqual(calculator_made_with_love.subtraction(-2, 2), -4)
+        self.assertEqual(calculator_made_with_love.subtraction(-2, -2), 0)
+
+    def test_multiplication(self):
+        self.assertEqual(calculator_made_with_love.multiplication(2, 2), 4)
+        self.assertEqual(calculator_made_with_love.multiplication(-2, 2), -4)
+        self.assertEqual(calculator_made_with_love.multiplication(-2, 0),0 )
+        
+    def test_division(self):
+        self.assertEqual(calculator_made_with_love.division(2, 2), 1)
+        self.assertEqual(calculator_made_with_love.division(-2, 2), -1)
+        self.assertEqual(calculator_made_with_love.division(1, 2),0.5 )
+
+        with self.assertRaises(ValueError):
+            calculator_made_with_love.division(2, 0)
+
+    
+            
+
+
+
+if __name__ == "__main__":
+    unittest.main()
