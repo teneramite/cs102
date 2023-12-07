735d1bf55dd8deda8ba986c6cbdd31e5665549a5
teneramite <kiselsofia3@gmail.com>
2023-12-07 14:58:17 +0300

    Вернули калькулятор!

---
+++ src/lab1/calculator.py
@@ -0,0 +1,35 @@
+import math
+
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError('--Division by zero is not allowed--')
+    return a / b
+
+'''while True:
+    a = float(input("Enter the first number>>"))
+    op = input("Enter an operation to perform>>")   
+    b = float(input("Enter the second number>>"))
+
+    if op == "+":
+        print(addition(a, b))
+    elif op == "-":
+        print(subtraction(a, b))
+    elif op == "*":
+        print(multiplication(a, b))
+    elif op == "/":
+        if b == 0:
+            print("Division by zero is not allowed")
+        else:
+             print(division(a, b))     
+
+    else:
+        print("Error: operation is not available")'''
